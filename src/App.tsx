a942a554d429faf2ebbe6dbb4695a9a50bcb0b05
skornya <kornyakovS@gmail.com>
2024-01-17 13:15:57 +0300

    prepare

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,5 @@
-import { useState } from 'react';
-
 import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div className="card-container">
-      <div className="card">
-        <div
-          className="text-box"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          <a href="#" className="btn btn-white btn-animate">
-            count is {count}
-          </a>
-        </div>
-      </div>
-    </div>
-  );
-}
+const App = () => <></>;
 
 export default App;
